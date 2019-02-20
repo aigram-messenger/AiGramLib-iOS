4560f45938d04964911cc41c9dd6f7383444135b
Dmitry Shelonin <dshelonin@smedialink.com>
2019-02-20 10:58:59 +0300

    - initial commit;

---
+++ AiGramLib/AiGramLib.h
@@ -0,0 +1,19 @@
+//
+//  AiGramLib.h
+//  AiGramLib
+//
+//  Created by Dmitry Shelonin on 20/02/2019.
+//  Copyright © 2019 Ol Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AiGramLib.
+FOUNDATION_EXPORT double AiGramLibVersionNumber;
+
+//! Project version string for AiGramLib.
+FOUNDATION_EXPORT const unsigned char AiGramLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AiGramLib/PublicHeader.h>
+
+
